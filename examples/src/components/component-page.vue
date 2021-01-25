2014a6e4f37615e0c8a4fdbcd494cdf3d7b1c8a8
JoeWrights <1035208578@qq.com>
2021-01-25 18:48:38 +0800

    feat(*): update examples

--- examples/src/components/component-page.vue
+++ examples/src/components/component-page.vue
@@ -9,15 +9,29 @@
             <demo1 slot="demo"></demo1>
             <code-reader slot="code" file="demo1.vue"></code-reader>
           </demo-code>
+
           <demo-code>
             <demo2 slot="demo"></demo2>
             <code-reader slot="code" file="demo2.vue"></code-reader>
             <md-reader slot="desc">通过`height`属性控制显示多少行省略</md-reader>
           </demo-code>
+
           <demo-code>
             <demo3 slot="demo"></demo3>
             <code-reader slot="code" file="demo3.vue"></code-reader>
-            <md-reader slot="desc">通过`after`插槽控制展开收起</md-reader>
+            <md-reader slot="desc">通过`after`插槽，可以自定义展开收起按钮，通过控制`isLimitHeight`属性来实现展开收起</md-reader>
+          </demo-code>
+
+          <demo-code>
+            <demo4 slot="demo"></demo4>
+            <code-reader slot="code" file="demo4.vue"></code-reader>
+            <md-reader slot="desc">也可以加js原生事件来实现展开收起 **例子1: 鼠标移入移出**</md-reader>
+          </demo-code>
+
+          <demo-code>
+            <demo5 slot="demo"></demo5>
+            <code-reader slot="code" file="demo5.vue"></code-reader>
+            <md-reader slot="desc">js原生事件来实现展开收起 **例子2: 鼠标点击**</md-reader>
           </demo-code>
         </template>
       </doc-section>
@@ -34,6 +48,8 @@ import CodeReader from './code-reader.vue'
 import Demo1 from '../demos/demo1.vue'
 import Demo2 from '../demos/demo2.vue'
 import Demo3 from '../demos/demo3.vue'
+import Demo4 from '../demos/demo4.vue'
+import Demo5 from '../demos/demo5.vue'
 
 export default {
   name: 'ComponentPage',
@@ -41,6 +57,8 @@ export default {
     Demo1,
     Demo2,
     Demo3,
+    Demo4,
+    Demo5,
     DocTitle,
     DocSection,
     DemoCode,
